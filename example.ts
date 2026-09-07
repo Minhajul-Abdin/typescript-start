c58f9751113d06ea6cab39545a3521082bf1205e
Minhajul Abdin <minhajul000abdin@gmail.com>
2026-09-08 04:28:56 +0600

    learned some types which we can use in typescript

--- example.ts
+++ example.ts
@@ -6,3 +6,14 @@ function divide(a: number,b: number): number{
 }
 
 console.log(divide(6,2));
+
+// if the function isn't returning anything then the type will be void
+
+function milti(a: number,b: number): void{ // nothing is going to be returned so the type is void
+    if(b == 0){
+        throw new Error("division is not possible");
+    }
+    const total = a+b;
+}
+
+console.log(milti(6,2));//nothing is being returned so the output is undifined
---
+++ types.ts
@@ -0,0 +1,9 @@
+const country: string = "Bangladesh";
+const n: number = 15;
+const trueOrFalse: boolean = true;
+
+let x;
+console.log(typeof x); //It will return undifined as we didn't assign any value or type
+
+//givng type to arrays
+const countries : string[] = ["Bangladesh","China","Saudi","india"];
